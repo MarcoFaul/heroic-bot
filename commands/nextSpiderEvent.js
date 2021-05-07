cf33daad107e2a32b73752e9f4949fb197836d2d
marcofaul <marcofaul@protonmail.com>
2021-05-07 19:46:39 +0200

    change next spider event command

--- commands/nextSpiderEvent.js
+++ commands/nextSpiderEvent.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 
-
 module.exports = {
     name: "nextevent",
     aliases: ["next", "nextinfo", "nexthero", "infopls", "gimmenexthero"],
@@ -16,11 +15,59 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
         embed.setTitle('Spider event overview');
         embed.addField(`**Next hero**:`, `\n\`${client.heroes.get(client.config.nextSpiderEventHero).name}\``, true)
-        embed.addField(`**Solo starts**:`, `\n\`${client.config.soloSpiderEventStart}\``, true)
-        embed.addField(`**Guild starts**:`, `\n\`${client.config.guildSpiderEventStart}\``, true)
+        embed.addField(`**Next guild attack in**:`, `\n\`${getNextGuildCountdown(client)}\``, true)
         embed.addField(`**Hero order**:`, `\n\`${heroOrder}\``, true)
         embed.addField(`**Best advices**:`, `\n\https://www.youtube.com/channel/UCEGGisfbz18_ZoPqH607sdg/featured`, true)
 
         message.reply(embed);
     }
+}
+
+const getText = (num) => {
+
+    if (num === 99999999) {
+        return 'Next guild attack time will be announced soon!'
+    }
+
+    var hours = (num / 60);
+    var rhours = Math.floor(hours);
+    var minutes = (hours - rhours) * 60;
+    var rminutes = Math.round(minutes);
+
+    if ((rhours === 0 && rminutes === 0) || rhours === -0 && rminutes === -0) {
+        return 'Attack now!';
+    }
+
+    return `${rhours}h ${rminutes}m`;
+}
+
+const getNextGuildCountdown = (client) => {
+    let message = `¯\\_(ツ)_/¯`;
+
+    client.guildAttackReminders.forEach((nextGuildAttack, key) => {
+        let nearestEvent = -99999999;
+
+        nextGuildAttack.forEach(nextAttack => {
+            tempStart = new Date(client.config.guildSpiderEventStart);
+            tempCurrentDate = new Date();
+            tempStart.setMinutes(tempStart.getMinutes() + nextAttack.start);
+
+            // get the different in minutes
+            let difference = tempCurrentDate.getTime() - tempStart.getTime();
+            let resultInMinutes = Math.round(difference / 60000);
+
+            // we want to get the lowest negative number for the next attack
+            if (Math.sign(resultInMinutes) !== -1) {
+                return;
+            }
+
+            if (nearestEvent < resultInMinutes) {
+                nearestEvent = resultInMinutes
+            }
+        })
+
+        message = getText(Math.abs(nearestEvent));
+    })
+
+    return message;
 }
